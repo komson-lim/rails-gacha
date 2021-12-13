925ed19dd31662d7909dc531a71cfa29ca3f1798
komson <shootingstar7447@gmail.com>
2021-12-13 12:58:49 +0700

    Add failed test case

--- test/system/main_test.rb
+++ test/system/main_test.rb
@@ -8,6 +8,13 @@ class MainTest < ApplicationSystemTestCase
         click_on "Login"
         assert_selector "h1", text: "Banner"
     end
+    test "login fail" do
+        visit "/main"
+        fill_in "Username", with: "asdf"
+        fill_in "Password", with: "asdf"
+        click_on "Login"
+        assert_selector "h1", text: "Login"
+    end
     test "register" do
         visit "/register"
         fill_in "Username", with: "nnn"
@@ -24,6 +31,16 @@ class MainTest < ApplicationSystemTestCase
         find('#banner-1').click_on("Like")
         visit "/favorite"
         assert has_css?('#banner-1')
+    end
+    test "unlike banner" do
+        visit "/main"
+        fill_in "Username", with: "ddd"
+        fill_in "Password", with: "ddd"
+        click_on "Login"
+        find('#banner-1').click_on("Like")
+        find('#banner-1').click_on("Unlike")
+        visit "/favorite"
+        assert !has_css?('#banner-1')
     end
     test "roll success" do
         visit "/main"
@@ -43,6 +60,17 @@ class MainTest < ApplicationSystemTestCase
         after_entries = find('#inv_info').text.match(/(\d+) entries/i).captures[0].to_i
         assert_equal 1, after_entries - before_entries
     end
+    test "roll fail not enough credit" do
+        visit "/main"
+        fill_in "Username", with: "ddd"
+        fill_in "Password", with: "ddd"
+        click_on "Login"
+        visit "/banner"
+        find('#banner-3 > a').click
+        find('#roll1').click
+        assert_equal "Failed to roll this banner", find("#modalTitle").text
+        assert_equal "Reason: Not have enough credit", find("#roll-body").text
+    end
     test "roll10 success" do
         visit "/main"
         fill_in "Username", with: "ddd"
@@ -61,6 +89,17 @@ class MainTest < ApplicationSystemTestCase
         after_entries = find('#inv_info').text.match(/(\d+) entries/i).captures[0].to_i
         assert_equal 10, after_entries - before_entries
     end
+    test "roll10 fail not enough credit" do
+        visit "/main"
+        fill_in "Username", with: "ddd"
+        fill_in "Password", with: "ddd"
+        click_on "Login"
+        visit "/banner"
+        find('#banner-3 > a').click
+        find('#roll10').click
+        assert_equal "Failed to roll this banner", find("#modalTitle").text
+        assert_equal "Reason: Not have enough credit", find("#roll-body").text
+    end
     test "sell item success" do
         visit "/main"
         fill_in "Username", with: "ddd"
@@ -75,6 +114,19 @@ class MainTest < ApplicationSystemTestCase
         visit "/market"
         after_entries = find('#inv_info').text.match(/(\d+) entries/i).captures[0].to_i
         assert_equal 1, after_entries - before_entries
+    end
+    test "unsell success" do
+        visit "/main"
+        fill_in "Username", with: "ddd"
+        fill_in "Password", with: "ddd"
+        click_on "Login"
+        visit "/inventory"
+        find("#item-id-1").click_on("sell")
+        fill_in "price", with: 5
+        find("#sellButton").click
+        visit "/inventory"
+        find("#item-id-1").click_on("unsell")
+        assert_equal "sell", find("#item-id-1 > form > input").value
     end
     test "buy item success" do
         visit "/main"
@@ -112,6 +164,15 @@ class MainTest < ApplicationSystemTestCase
         seller_after_entries = find('#inv_info').text.match(/(\d+) entries/i).captures[0].to_i
         assert_equal 1, seller_before_entries - seller_after_entries
     end
+    test "buy item fail not enough credit" do
+        visit "/main"
+        fill_in "Username", with: "ddd"
+        fill_in "Password", with: "ddd"
+        click_on "Login"
+        visit "/market"
+        find("#item-id-3").click_on("buy")
+        assert_equal "Not enough credit :(", find(".alert").text
+    end
     test "redeem success" do
         visit "/main"
         fill_in "Username", with: "ddd"
@@ -123,5 +184,15 @@ class MainTest < ApplicationSystemTestCase
         click_on "Submit"
         after_credit = find('#credit').text.match(/credit: (\d+)/i).captures[0].to_i
         assert_not_equal before_credit, after_credit
+    end
+    test "redeem fail" do
+        visit "/main"
+        fill_in "Username", with: "ddd"
+        fill_in "Password", with: "ddd"
+        click_on "Login"
+        visit "/redeem"
+        fill_in "code", with: "sadfsadf"
+        click_on "Submit"
+        assert_equal "Failed to redeem the code", find(".alert").text
     end
 end
