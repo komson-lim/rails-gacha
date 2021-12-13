1fef98f6b1f887e864a21e6ceda103024ef3c083
komson <shootingstar7447@gmail.com>
2021-12-13 09:59:46 +0700

    Subtitute rarity for stars

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,6 +1,7 @@
 class Inventory < ApplicationRecord
   belongs_to :user
   belongs_to :item
+  validates_numericality_of :price, :greater_than_or_equal_to =>0 , message: "Cannot be negative number"
   def self.getSell
     Item.joins(:inventories).where("inventories.status = 'sell'").select("items.id,items.name,items.rarity,inventories.id as iid, inventories.status, inventories.price, inventories.updated_at, inventories.user_id, items.url").order("inventories.updated_at DESC")
   end
